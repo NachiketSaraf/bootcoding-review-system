40d8997df696fd865120c9bf078498b86d892006
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-08 23:35:09 +0530

    review system

---
+++ review.system/src/main/java/com/bootcoding/review/review/system/Application.java
@@ -0,0 +1,13 @@
+package com.bootcoding.review.review.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
