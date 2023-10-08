266df8650047d080716e98f219c36b7eb180cebe
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-08 23:36:25 +0530

    review system

---
+++ review.system/src/main/java/com/bootcoding/review/review/system/model/User.java
@@ -0,0 +1,12 @@
+package com.bootcoding.review.review.system.model;
+
+import lombok.Data;
+
+@Data
+
+public class User {
+    private String name;
+    private String emailId;
+    private long number;
+
+}
