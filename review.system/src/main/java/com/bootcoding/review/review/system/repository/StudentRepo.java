e1fbfc407b35fb9bae6f99e06cdd3c53f786c78a
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-14 00:13:57 +0530

    Spring student project

---
+++ review.system/src/main/java/com/bootcoding/review/review/system/repository/StudentRepo.java
@@ -0,0 +1,21 @@
+package com.bootcoding.review.review.system.repository;
+
+import com.bootcoding.review.review.system.model.Student;
+
+import java.util.List;
+
+public interface StudentRepo {
+    Student insertStudent(Student student);
+
+    Student getStudent(int rollNumber);
+
+    List<Student> getStudentList();
+
+    Student updateStudent(int studentId, Student student);
+
+    String deleteStudent(int studentId);
+
+    List<Student> getStudentsByBranch(String branch);
+
+    boolean checkStudent(Student student);
+}
