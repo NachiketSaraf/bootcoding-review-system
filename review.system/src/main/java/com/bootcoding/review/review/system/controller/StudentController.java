4e1f240ffca041aae3db18a9e75b7b4e4ddc948f
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-13 23:54:43 +0530

    Spring student project

---
+++ review.system/src/main/java/com/bootcoding/review/review/system/controller/StudentController.java
@@ -0,0 +1,53 @@
+package com.bootcoding.review.review.system.controller;
+
+import com.bootcoding.review.review.system.model.Student;
+import com.bootcoding.review.review.system.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/student")
+    public String insertStudent(@RequestBody Student student) {
+        if(studentService.studentExist()){
+            return "Student exist";
+        }
+        return studentService.insertStudent(student);
+    }
+
+    @GetMapping("/student/{rollNumber}")
+    public Student getStudent(@PathVariable int rollNumber) {
+        return studentService.getStudent(rollNumber);
+    }
+
+    @GetMapping("/student")
+    public List<Student> getStudentList() {
+        return studentService.getStudentList();
+    }
+
+    @PutMapping("/student/{studentId}")
+    public Student updateStudent(@PathVariable int studentId, @RequestBody Student student) {
+        return studentService.updateStudent(studentId, student);
+    }
+
+    @DeleteMapping("/student/{studentId}")
+    public String deleteStudent(@PathVariable int studentId) {
+        return studentService.deleteStudent(studentId);
+    }
+
+
+    @GetMapping("/student/branch/{branch}")
+    public List<Student> getStudentsByBranch(@PathVariable String branch) {
+        return studentService.getStudentsByBranch(branch);
+
+
+    }
+
+}
