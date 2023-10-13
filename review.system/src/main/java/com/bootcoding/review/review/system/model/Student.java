79f0980c295e750d6c567d27ffa9c52e25d8fa57
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-13 23:54:23 +0530

    Spring student project

---
+++ review.system/src/main/java/com/bootcoding/review/review/system/model/Student.java
@@ -0,0 +1,19 @@
+package com.bootcoding.review.review.system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int rollNumber;
+    private String name;
+    private int age;
+    private String gender;
+    private String branch;
+    private int year;
+    private int semister;
+}
